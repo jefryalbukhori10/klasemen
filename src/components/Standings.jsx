2223de3e49f2c388a0a00dffbf4b697c750355bd
jefryalbukhori10 <jefryalbukhori23@gmail.com>
2026-07-10 18:23:45 +0700

    update

--- src/components/Standings.jsx
+++ src/components/Standings.jsx
@@ -77,7 +77,7 @@ export default function Standings({ groups, matches, criteria }) {
   const groupKeys = Object.keys(groups).sort()
   return (
     <div className="space-y-6">
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 sm:grid-cols-1 lg:grid-cols-1">
         {groupKeys.map((g) => (
           <StandingsTable
             key={g}
